2daca4c871528f4aab17449ac7865daa898d8435
eomidkhoda <elahehomid@gmail.com>
2024-12-30 12:14:04 +0330

    add TsolesPlugin folder and create base classes

---
+++ src/TSolesPlugin/job.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TSolesPlugin.Job
+// Assembly: TSolesPlugin, Version=3.4.1.0, Culture=neutral, PublicKeyToken=null
+// MVID: 943EFD72-12B6-4D53-BB0D-FD8C4C742C73
+// Assembly location: D:\opt directory (T-Soles)\dsf\bin\TSolesPlugin.dll
+
+#nullable enable
+namespace TSolesPlugin
+{
+    public class Job
+    {
+        public string Filename { get; set; } = string.Empty;
+
+        public bool Active { get; set; }
+
+        public bool ExternallyStarted { get; set; }
+
+        public int Completed { get; set; }
+
+        public int Total { get; set; }
+
+        public long? PrintTime { get; set; }
+    }
+}
